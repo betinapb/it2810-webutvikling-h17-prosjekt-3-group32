ff246c48c4a8c78b85e78b3318552979bd088c13
bujordet <mo.bujordet@gmail.com>
2017-10-03 15:16:37 +0200

    Creates form and adds design for form

--- totater/src/App.js
+++ totater/src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import Notes from 'containers';
 class App extends Component {
-  render() {
-    return (
-      <Notes />
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title">Mine notater</h1>
+                </header>
+                <Notes />
+            </div>
+        );
+    }
 }
 
 export default App;
--- totater/src/containers/Notes.js
+++ totater/src/containers/Notes.js
@@ -1,19 +1,68 @@
 import React, { Component } from 'react';
 import 'App.css';
+import Textarea from 'react-textarea-autosize';
 
 class Notes extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Mine notater</h1>
-        </header>
-        <p className="App-intro">
-          Her kan man legge inn notater
-        </p>
-      </div>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: '',
+            note: ''
+        };
+
+        this.onChange = this.onChange.bind(this);
+        this.onNoteChange = this.onNoteChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onChange(e) {
+        this.setState({ title: e.target.value });
+    }
+
+    onNoteChange(e) {
+        this.setState({ note: e.target.value });
+    }
+
+    onSubmit(e) {
+        //Save to HTML Local Storage
+        alert("hei")
+        e.preventDefault();
+    }
+
+    render() {
+        return (
+            <div className="Notes-container">
+                <p className="App-intro">
+                    Her kan man legge inn notater
+                </p>
+                <div className="Form-container">
+                    <form onSubmit={this.onSubmit}>
+                        <div className="Label-box">
+                            <label>
+                            <input
+                                placeholder="Title"
+                                onChange={this.onChange}
+                            />
+                            </label>
+                        </div>
+                        <div className="Text-box">
+                            <Textarea
+                                minRows={2}
+                                maxRows={6}
+                                placeholder="Take a note..."
+                                onChange={this.onNoteChange}
+                                maxLength='150'
+                            />
+                        </div>
+                        <div className="Button-box">
+                            <input type="submit" value="DONE" />
+                        </div>
+                    </form>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Notes;
